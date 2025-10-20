bf16684c2b0c49042f6a927ab69c74ce52ed67f5
Tamar <tamarnatchkebia2@gmail.com>
2025-10-20 14:59:15 +0400

    feat/ui: add colors, fonts and base components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <div className="site-wrapper">{children}</div>
       </body>
     </html>
   );
